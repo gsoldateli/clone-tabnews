67a9d7f06efe4fe870c24e2f563858eb893f321f
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2025-12-12 10:24:36 +0000

    Create  connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+async function status(request, response) {
+  const result = await database.query("SELECT 2221+1 as meu_amigo;");
+  console.log({ result: result.rows });
   response.status(200).json({ status: "chuvá" });
 }
 
