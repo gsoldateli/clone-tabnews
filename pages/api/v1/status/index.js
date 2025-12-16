7b6cfcad59535259dc4fb8e5b26fe9cf238327d7
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2025-12-16 11:00:46 +0000

    finalize database status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,35 @@
 import database from "/infra/database.js";
 async function status(request, response) {
-  const result = await database.query("SELECT 2221+1 as meu_amigo;");
-  console.log({ result: result.rows });
-  response.status(200).json({ status: "chuvá" });
+  const databaseName = process.env.POSTGRES_DB;
+
+  const updatedAt = new Date().toISOString();
+
+  const result = await database.query("SHOW server_version;");
+  const databaseVersion = result.rows[0].server_version;
+
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnections = resultMaxConnections.rows[0].max_connections;
+
+  const resultStatActivityConnections = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const openedDatabaseConnections = parseInt(
+    resultStatActivityConnections.rows[0].count,
+    10,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections, 10),
+        opened_connections: openedDatabaseConnections,
+      },
+    },
+  });
 }
 
 export default status;
